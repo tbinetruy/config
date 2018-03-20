6b68eb19f95f92c940c7d0fcd3aa0fbe0bf70ce1
Thomas Binetruy <tbinetruy@gmail.com>
2018-03-20 16:36:52 -0400

    cleaned up flow example

--- spacemacs/flow/example/flow.js
+++ spacemacs/flow/example/flow.js
@@ -2,17 +2,15 @@
 
 
 
-function foo(): typeof AA { return AA }
-
 const AA = {a: 'hello', b: [1, 2, 3]}
-
-
-
-
+function foo(): typeof AA { return AA }
 
 let bar = foo()
 const b = bar.b.map(e => e)
 
 let a: string = bar.a
+
+const f = (x: number, y: number): number => x * y
+f(1, 2)
 
 export default {a, b}
