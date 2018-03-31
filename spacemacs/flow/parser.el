ddb07601b287a96856f080e75e1506534e5c96e2
Thomas Binetruy <tbinetruy@gmail.com>
2018-03-31 18:23:14 -0400

    renamed functions

--- spacemacs/flow/parser.el
+++ spacemacs/flow/parser.el
@@ -1,8 +1,8 @@
 
-(defun parse/is-digit (c)
+(defun lexer/is-digit (c)
   (string-match "[0-9]" (format "%c" c)))
 
-(defun parse/read-digit ()
+(defun lexer/read-digit ()
   (let ((current-point (point))
         (match-end-pos (re-search-forward "\\([0-9]*\.\\)?[0-9]+"))
         (read-value ""))
@@ -14,10 +14,10 @@
              (value . ,read-value))))
       (nil))))
 
-(defun parse/is-type (c)
+(defun lexer/is-type (c)
   (string-match "[a-zA-Z0-9\$]+" (format "%c" c)))
 
-(defun parse/read-type ()
+(defun lexer/read-type ()
   (let ((current-point (point))
         (match-end-pos (re-search-forward "[a-zA-Z0-9]+"))
         (read-value ""))
@@ -29,10 +29,10 @@
              (value . ,read-value))))
       (nil))))
 
-(defun parse/is-special-char (c)
+(defun lexer/is-special-char (c)
   (string-match "[{}\\:=<>,]" (format "%c" c)))
 
-(defun parse/read-special-char ()
+(defun lexer/read-special-char ()
   (let ((current-point (point))
         (read-value ""))
     (setq read-value (format "%c"
@@ -40,13 +40,13 @@
     `(((type . "special-char")
        (value . ,read-value)))))
 
-(defun parse (str)
+(defun lexer/lex (str)
   (let ((output nil)
         (input nil)
         (current-character nil)
         (counter nil)
         (keywords '("type")))
-    (defun parse/test-character (predicate success-callback backward-on-success)
+    (defun lexer/test-character (predicate success-callback backward-on-success)
       (if (and (funcall predicate current-character)
                (not counter))
           (progn
@@ -55,12 +55,12 @@
                           (funcall success-callback)))
             (backward-char backward-on-success))
         nil))
-    (defun parse/top-level-parsing ()
-      (parse/test-character 'parse/is-digit 'parse/read-digit
+    (defun lexer/top-level-parsing ()
+      (lexer/test-character 'lexer/is-digit 'lexer/read-digit
                             1)
-      (parse/test-character 'parse/is-special-char
-                            'parse/read-special-char 0)
-      (parse/test-character 'parse/is-type 'parse/read-type
+      (lexer/test-character 'lexer/is-special-char
+                            'lexer/read-special-char 0)
+      (lexer/test-character 'lexer/is-type 'lexer/read-type
                             1)
       (if counter
           (setq counter nil)
@@ -71,11 +71,9 @@
       (goto-char (point-min))
       (while (not (eobp))
         (setq current-character (char-after))
-        (parse/top-level-parsing)
+        (lexer/top-level-parsing)
         (forward-char 1))
       (setq input (buffer-string)))
     (format "%s" output)))
 
-(provide 'parse)
-
-(message (parse "{foo: 1.122, bar: foobar}"))
+(message (lexer/lex"{foo: 1.122, bar: foobar}"))
