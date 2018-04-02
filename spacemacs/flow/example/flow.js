214394cce48eae0dafa919ffdce078de3ee2ca68
Thomas Binetruy <tbinetruy@gmail.com>
2018-04-02 17:18:09 -0400

    added notes

--- spacemacs/flow/example/flow.js
+++ spacemacs/flow/example/flow.js
@@ -15,10 +15,32 @@ f(1, 2)
 
 type Test<RS> = Array<RS>
 let what: Test<string> = ["hello"]
-type Test2 = Test & Array<string>
-
-what
+type Test2 = (Test<number> & Array<Array<string>>) | boolean
+type Test3 = Test2 | number
+type Test4 = [number, string, Array<string>]
 
 let what2: Test2
+
+interface Serializable {
+    serialize(): string;
+}
+
+class Foo implements Serializable {
+    serialize() { return '[Foo]' }
+}
+
+class Bar implements Serializable {
+    serialize() { return 'Bar' }
+}
+
+const yay1 = new Foo()
+const yay = new Bar()
+
+let aaaa: typeof yay1
+
+let val = (value: number);
+let obj = { prop: (value: Type) };
+let arr = ([(value: Type), (value: Type)]: Array<Type>);
+
 
 export default {a, b}
