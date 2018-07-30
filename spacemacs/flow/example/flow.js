edd0b28e2b77587acdbee90ff6299e14dd9d44d1
Thomas Binetruy <tbinetruy@gmail.com>
2018-07-30 12:27:31 -0400

    updated flow

--- spacemacs/flow/example/flow.js
+++ spacemacs/flow/example/flow.js
@@ -1,5 +1,10 @@
 // @flow
 
+import React from 'react'
+import zzz, {
+    C,
+} from './extra.js'
+
 interface Serializable {
     serialize(): string;
 }
@@ -15,8 +20,31 @@ class Bar {
 const b = new Bar()
 const f = (a = true) => false
 
-let foo: number
+let foo: Array<{
+    a: number,
+    a1: number,
+    a2: number,
+    a3: number,
+    a4: number,
+    a5: number,
+    a6: number,
+    a7: number,
+    a8: number,
+    b: () => void
+}>
+
 foo = false
+console.log('yo')
+console.log('yo')
+console.log('yo')
+console.log('yo')
+console.log('yo')
+console.log('yo')
+console.log('yo')
+console.log('yo')
+console.log('yo')
+console.log('yo')
+console.log('yo')
 
 type F$C<T> = T
 
@@ -24,3 +52,17 @@ let A = {
     foo: 1,
     bar: true,
 }
+
+const hey = props => <div>{ 1 + 1}</div>;
+let _sleeping = zzz
+
+const sleeping: string = {
+    a: zzz
+}
+
+let foooo = (a) => console.log(a)
+foooo("1")
+
+console.log()
+
+let c = new C()
